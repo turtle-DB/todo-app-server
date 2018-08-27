f9f50f768bf9e21d261b75bf12a46726ae2dc4c9
Max Appleton <maxiappleton@gmail.com>
2018-08-27 15:24:08 -0500

    basic setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const TortoiseDB = require('tortoisedb');
+
+const db = new TortoiseDB({
+  name: 'todo-app',
+  port: 3001,
+  mongoURI: 'mongodb://localhost:27017',
+  batchLimit: 1000
+});
+
+db.start();
+
+// Every 10 mins
+setInterval(db.dropDB.bind(db), 600000);
